745403a238cc767213f4bf27d64a10e742aeff6a
Oscar Palomino <oscar_epalomino@soy.sena.edu.co>
2025-04-20 16:35:37 -0500

    Primer componente creado

---
+++ src/PrimerComponente.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PrimerComponente() {
+    return (
+        <div className="primerComponente">
+            <h2>Primer Componente</h2>
+            <p>¡Hola Mundo! Este es mi primer componente</p>
+        </div>
+    )
+}
+
+export default PrimerComponente;
